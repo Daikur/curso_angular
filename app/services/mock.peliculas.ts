72873009b8267c4e5a1b5fdd6c236cc135f2d9a9
Daikur <ortegasanchezdavid@hotmail.com>
2016-12-04 13:46:31 +0100

    Curso Angular Terminado

---
+++ app/services/mock.peliculas.ts
@@ -0,0 +1,10 @@
+import {Pelicula} from "../model/pelicula";
+
+export const PELICULAS: Pelicula[] = [
+  new Pelicula(1, "Batman v Superman","Zack Snider",2016),
+  new Pelicula(2, "La verdad Duele","Will Smith",2016),
+  new Pelicula(3, "El seños de los anillos","Desconocido", 2002),
+  new Pelicula(4, "Una historia real","El de supersalidos",2015),
+  new Pelicula(5, "Don Jon","Josep Gordon Levir", 2004),
+  new Pelicula(6, "Dos tontos muy tontos","Desconocido", 2001)
+];
